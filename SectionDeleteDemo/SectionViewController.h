e6b02b54f059047baf1280c1b48614d8eab3a25b
Marco Abundo <shrtlist@gmail.com>
2013-09-16 14:34:08 -0700

    Initial commit

---
+++ SectionDeleteDemo/SectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  SectionViewController.h
+//  SectionDeleteDemo
+//
+//  Created by Marco Abundo on 9/16/13.
+//  Copyright (c) 2013 Your company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SectionViewController : UITableViewController
+
+@end
